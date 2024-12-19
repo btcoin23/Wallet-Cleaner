36d86856d4b317257f4c435617a8b45a068eb023
Solana <138183918+btcoin23@users.noreply.github.com>
2024-12-20 05:56:28 +0900

    update config

--- config.ts
+++ config.ts
@@ -2,6 +2,5 @@ import { Connection } from '@solana/web3.js'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const RPC_URL = process.env.RPC_URL||'https://api.mainnet-beta.solana.com'
-'
+const RPC_URL = process.env.RPC_URL||'https://api.mainnet-beta.solana.com';
 export const connection = new Connection(RPC_URL)
