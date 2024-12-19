20989ea4ee658bb928a8c5ab6e8cc604bd092d01
Solana <138183918+btcoin23@users.noreply.github.com>
2024-12-20 05:51:52 +0900

    add default rpc

--- config.ts
+++ config.ts
@@ -2,5 +2,6 @@ import { Connection } from '@solana/web3.js'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const RPC_URL = process.env.RPC_URL||''
+const RPC_URL = process.env.RPC_URL||'https://api.mainnet-beta.solana.com'
+'
 export const connection = new Connection(RPC_URL)
